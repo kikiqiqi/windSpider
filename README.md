fb7480caba2587b05a616c32d6ba22459359e29f
zhongqi <593373649@qq.com>
2019-01-02 14:12:04 +0800

    read the bonds name and write the wind code

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # windSpider
 ---
 - wind_code.py is the function to get the wind code
+- bonds_read.py is the main function to read the bonds name and write the wind code
+- bonds.csv is the bond name
+- tag_code.csv is the result of bond name to wind code
---
+++ bonds_read.py
@@ -0,0 +1,20 @@
+# -*-coding:utf-8 -*-
+import pandas as pd
+from wind_code import getWindCode
+
+if __name__ == "__main__":
+    bonds = pd.read_csv("bonds.csv")
+    bonds_lst = bonds["债券名称"].tolist()
+
+    offset = 0
+
+    f = open("tag_code_csv.csv", "a")
+    for i in range(offset, len(bonds_lst)):
+        tag_code = getWindCode(bonds_lst[i])
+        if tag_code:
+            f.write(bonds_lst[i] + "," + tag_code[0] + "\n")
+        else:
+            break
+
+    print(i)
+    f.close()
